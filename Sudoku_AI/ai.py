19152313ab1e90babc9b6ac6d3f5272bd9b794bf
Saeid Cheshmi <saeidcheshmi@outlook.com>
2022-12-30 23:45:00 +0330

    ai.py

---
+++ Sudoku_AI/ai.py
@@ -0,0 +1,24 @@
+import json
+
+
+# *** you can change everything except the name of the class, the act function and the problem_data ***
+
+
+class AI:
+    # ^^^ DO NOT change the name of the class ***
+
+    def __init__(self):
+        pass
+
+    # the solve function takes a json string as input
+    # and outputs the solved version as json
+    def solve(self, problem):
+        # ^^^ DO NOT change the solve function above ***
+
+        problem_data = json.loads(problem)
+        # ^^^ DO NOT change the problem_data above ***
+
+        # TODO implement your code here
+
+        # finished is the solved version
+        return finished
